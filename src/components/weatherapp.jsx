3c1d0d8c02458a83a7fd11d1f402954a4d2fdce0
ToshiM <toshim0017@gmail.com>
2024-03-07 23:38:26 +0100

    poprawa wizualna, dodano termometr

--- src/components/weatherapp.jsx
+++ src/components/weatherapp.jsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faCog } from '@fortawesome/free-solid-svg-icons'
 
 const Weatherapp = () => {
     let api_key = "9d4761c1f4ddf1a245e715f546407ee3";
@@ -18,33 +19,77 @@ const Weatherapp = () => {
         const pressure = document.getElementsByClassName("pressure");
         const humidity = document.getElementsByClassName("humidity");
         const wind = document.getElementsByClassName("wind");
-        const icon = document.getElementsByClassName("icon");
+        const weatherName = document.getElementsByClassName("weatherName");
 
         cityName[0].innerHTML = data.name;
-        temp[0].innerHTML = data.main.temp+" °C";
-        feelsLike[0].innerHTML = data.main.feels_like+" °C";
+        temp[0].innerHTML = data.main.temp+"°C";
+        feelsLike[0].innerHTML = data.main.feels_like+"°C";
         pressure[0].innerHTML = data.main.pressure+" hPa";
-        humidity[0].innerHTML = data.main.humidity+" %";
+        humidity[0].innerHTML = data.main.humidity+"%";
         wind[0].innerHTML = data.wind.speed+" m/s";
-        icon[0].innerHTML = "<img src='http://openweathermap.org/img/w/"+data.weather[0].icon+".png' alt='icon'>";
+        weatherName[0].innerHTML = data.weather[0].description;
+
+        const line = document.getElementsByClassName("line")[0];
+        line.style.height = `${(data.main.temp / 100) * 100 + 50}%`;
+        line.style.background = `linear-gradient(to bottom, ${getTemperatureColor(data.main.temp)}, #5555ff)`;
+
+        function getTemperatureColor(temperature) {
+            if (temperature >= 26) {
+                return "#ff0000";
+            } else if (temperature >= 18) {
+                return "#ff7c00";
+            } else if (temperature >= 12) {
+                return "#ffa500";
+            } else if (temperature >= 5) {
+                return "#ffeb3b";
+            } else if (temperature >= -5) {
+                return "#8fabff";
+            } else {
+                return "#6666dd";
+            }
+        }
     }
   return (
     <main>
-        <h1>Weather App</h1>
+        <header>
+            <h1>Pogoda</h1>
+            <button>
+                <FontAwesomeIcon icon={faCog} />
+            </button>
+        </header>
         <div className="search">
-            <input type="text" className="searchBar" placeholder="Search..." />
+            <input type="text" className="searchBar" placeholder="Wpisz nazwe miasta..." />
             <button onClick={()=>{search()}}>
                 <FontAwesomeIcon icon={faSearch} />
             </button>
         </div>
         <div className="location">
-            <div className="cityName">Nazwa</div>
-            <div className="temp">24</div>
-            <div className="feelsLike">23</div>
-            <div className="pressure">1010</div>
-            <div className="humidity">50</div>
-            <div className="wind">18</div>
-            <div className="icon">icon</div>
+            <div className="left">
+                <p className="cityName">Nazwa</p>
+                <div className="infoSet">
+                    <p>Ciśnienie: </p>
+                    <p className="pressure">1010</p>
+                </div>
+                <div className="infoSet">
+                    <p>Wilgotność: </p>
+                    <p className="humidity">50</p>
+                </div>
+                <div className="infoSet">
+                    <p>Wiatr: </p>
+                    <p className="wind">18</p>
+                </div>
+                <div className="infoSet">
+                    <p>Odczuwalna: </p>
+                    <p className="feelsLike">20</p>
+                </div>
+                <p className="temp">24</p>
+                <p className="weatherName">Pogoda</p>
+            </div>
+            <div className="right">
+                <div className="thermometer">
+                    <div className="line"></div>
+                </div>
+            </div>
         </div>
     </main>
   );
